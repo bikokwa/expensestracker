a3112d34a4a8778ba3a61be2129721fafb0e800a
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-31 18:33:02 +0300

    practicing conditional content on the form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,32 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import React, { useState } from "react";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
+  const closeFormHandler = () => {
+    setShowForm(false);
+  };
+
+  if (showForm) {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onClose={closeFormHandler}
+          onAddExpense={props.onAddExpense}
+        />
+      </div>
+    );
+  }
+
+  const showExpenseFormHandler = () => {
+    setShowForm(true);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onAddExpense={props.onAddExpense} />
+      <button onClick={showExpenseFormHandler}>Add New Expense</button>
     </div>
   );
 };
